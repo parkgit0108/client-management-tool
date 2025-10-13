2494cf0fccfdcfc699181f39c0e27f0cc269308b
Kyowon <kparkgit1053967@gmail.com>
2025-10-13 21:44:49 +1300

    add menu items and navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,7 +11,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT SIDE */}
-      <div className="w-[14%] md:w-[8%] lg:w-[14%] xl:w-[14%] bg-red-200 p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[14%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center lg:justify-start gap-2">
           <Image
             src="/avatar.png"
@@ -20,10 +22,12 @@ export default function DashboardLayout({
           />
           <span className="hidden lg:block">The Fitness Hub</span>
         </Link>
+        <Menu />
       </div>
       {/* RIGHT SIDE */}
-      <div className="w-[86%] md:w-[92%] lg:w-[86%] xl:w-[86%] bg-blue-200">
-        r
+      <div className="w-[86%] md:w-[92%] lg:w-[86%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
       </div>
     </div>
   );
